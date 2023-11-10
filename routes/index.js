3d527affe7882e0a621ec64f0ffce73904aba182
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-11-10 18:24:40 +0100

    Made API for inserting data into table 'kursna lista'

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,73 @@
 const express = require("express");
-const router = express.Router();
+const axios = require("axios");
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const databaseConfig = {
+const router = express.Router();
+
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-};
+  port: process.env.DB_PORT,
+});
 
-console.log("databaseConfig", process.env);
-
-const pool = mysql.createPool(databaseConfig);
-
-router.get("/", function (req, res, next) {
-  pool.getConnection((err, connection) => {
-    connection.query(
-      "SELECT * FROM zaposlenik_v2",
-      function (queryErr, results) {
-        connection.release();
-
-        if (queryErr) {
-          console.error("Error executing query:", queryErr);
-          return res.status(500).json({ error: "Internal Server Error" });
-        }
-
-        console.log("Query results:", results);
-      }
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      "https://cbbh.ba/CurrencyExchange/GetJson?date=" +
+        new Date().toISOString().slice(0, 10)
     );
-  });
+
+    if (response.status === 200) {
+      const { data: responseData } = response;
+      connection.connect();
+
+      const date = new Date(responseData.Date.slice(0, 10));
+      const data = {
+        date: date,
+        year: date.getFullYear(),
+        number: responseData.Number,
+      };
+
+      connection.query(
+        "INSERT INTO kursna_lista (datum_kursne_liste, godina, broj_kursne_liste) VALUES (?, ?, ?)",
+        [data.date, data.year, data.number],
+        function (error, results) {
+          if (error) throw error;
+
+          const exchangeRatesId = results.insertId;
+
+          for (const value of responseData.CurrencyExchangeItems) {
+            connection.query(
+              "INSERT INTO kurs_valute (kursna_lista_id, oznaka_valute, kod_valute, jedinica, kupovni_kurs, srednji_kurs, prodajni_kurs) VALUES (?, ?, ?, ?, ?, ?, ?)",
+              [
+                exchangeRatesId,
+                value.AlphaCode,
+                value.NumCode,
+                value.Units,
+                Number(value.Buy),
+                Number(value.Middle),
+                Number(value.Sell),
+              ],
+              function (error) {
+                if (error) throw error;
+              }
+            );
+          }
+
+          connection.end();
+        }
+      );
+      res.status(200).json({ message: "Successfully inserted data." });
+    } else {
+      res.status(500).json({ error: "Failed to fetch data." });
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
 });
 
 module.exports = router;
