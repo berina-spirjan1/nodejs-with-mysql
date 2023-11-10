5b9d9b256fbec95e7e7dd0f2cfdc11948458c673
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2023-11-10 21:58:34 +0100

    Created a new API with calling store procedure

--- routes/index.js
+++ routes/index.js
@@ -94,4 +94,105 @@ router.get("/lista-zaposlenika", function (req, res, next) {
   );
 });
 
+router.get("/kursna-lista-sp", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      "https://cbbh.ba/CurrencyExchange/GetJson?date=" +
+        new Date().toISOString().slice(0, 10)
+    );
+
+    if (response.status === 200) {
+      const { data: responseData } = response;
+
+      dbConnection.connect();
+
+      const date = new Date(responseData.Date.slice(0, 10));
+
+      const data = {
+        date: date,
+        year: date.getFullYear(),
+        number: responseData.Number,
+      };
+
+      dbConnection.query(
+        "CALL proc_dodaj_kursnu_listu(?, ?, ?, @p_kod_greske, @p_kursna_lista_id)",
+        [data.date, data.year, data.number, 0, 0],
+        function (error, results, fields) {
+          if (error) {
+            console.error("Error:", error.message);
+            dbConnection.rollback();
+            res.status(500).json({ error: "Failed to insert data." });
+            return;
+          }
+
+          dbConnection.query(
+            "SELECT @p_kod_greske, @p_kursna_lista_id",
+            function (selectError, selectResults) {
+              if (selectError) {
+                console.error("Error:", selectError.message);
+                dbConnection.rollback();
+                res.status(500).json({ error: "Failed to insert data." });
+                return;
+              }
+
+              const statusCode = selectResults[0]["@p_kod_greske"];
+              const kursnaListaId = selectResults[0]["@p_kursna_lista_id"];
+
+              if (statusCode !== 0) {
+                if (statusCode === 1) {
+                  res.status(400).json({
+                    message:
+                      "Exchange rate list with the given number already exists in the database.",
+                  });
+                } else if (statusCode === 2) {
+                  res.status(400).json({
+                    message:
+                      "Exchange rate list for the specified date already exists in the database.",
+                  });
+                }
+                dbConnection.rollback();
+                return;
+              }
+
+              for (const value of responseData.CurrencyExchangeItems) {
+                dbConnection.query(
+                  "INSERT INTO kurs_valute (kursna_lista_id, oznaka_valute, kod_valute, jedinica, kupovni_kurs, srednji_kurs, prodajni_kurs) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                  [
+                    kursnaListaId,
+                    value.AlphaCode,
+                    value.NumCode,
+                    value.Units,
+                    convertStrToNum(value.Buy),
+                    convertStrToNum(value.Middle),
+                    convertStrToNum(value.Sell),
+                  ],
+                  function (insertError) {
+                    if (insertError) {
+                      console.error("Error:", insertError.message);
+                      dbConnection.rollback();
+                      res.status(500).json({ error: "Failed to insert data." });
+                      return;
+                    }
+                  }
+                );
+              }
+
+              dbConnection.commit();
+              res.status(200).json({
+                message: "Successfully inserted data.",
+                status: 200,
+              });
+            }
+          );
+        }
+      );
+    } else {
+      res.status(500).json({ error: "Failed to fetch data." });
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
+});
+
 module.exports = router;
